71ef46e1d88a98dc8e69e43ca8e428a06cc94589
Cecilia <ccchimento@gmail.com>
2024-02-22 19:19:34 -0700

    completed lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,46 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = [message.to_dict() for message in Message.query.order_by(Message.created_at).all()]
+        return make_response(messages, 200)
+    
+    elif request.method == 'POST':
+        message_json = request.get_json()
+        
+        new_message = Message()
+        for key, value in message_json.items():
+            setattr(new_message, key, value)
+        
+        db.session.add(new_message)
+        db.session.commit()
 
-@app.route('/messages/<int:id>')
+        return make_response(new_message.to_dict(), 201)
+
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+    if not message:
+        return make_response({'error': 'This is not available, our bad.'}, 404)
+    else:
+        if request.method == 'GET':
+            return make_response(message, 200)
+        elif request.method == 'PATCH':
+            message_json = request.get_json()
 
+            for key, value in message_json.items():
+                setattr(message, key, value)
+            
+            db.session.commit()
+
+            return make_response(message.to_dict(), 202)
+        elif request.method == 'DELETE':
+            db.session.delete(message)
+            db.session.commit()
+
+            return make_response({}, 204)
+    
 if __name__ == '__main__':
     app.run(port=5555)
